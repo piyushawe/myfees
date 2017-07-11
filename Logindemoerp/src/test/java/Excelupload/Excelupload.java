dc5be0d08d546213fc40628f60786620fa47679f
piyushawe <piyush.nautiyal@franciscansolutions.in>
2017-07-11 16:36:20 +0530

    testing

--- Logindemoerp/src/test/java/Excelupload/Excelupload.java
+++ Logindemoerp/src/test/java/Excelupload/Excelupload.java
@@ -1,4 +1,4 @@
-package Excelupload;
+	package Excelupload;
 
 import java.io.File;
 import java.io.FileInputStream;
--- Logindemoerp/src/test/java/Logindem/Logindem.java
+++ Logindemoerp/src/test/java/Logindem/Logindem.java
@@ -3,9 +3,11 @@ package Logindem;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 
 public class Logindem{
 	
@@ -19,10 +21,10 @@ public class Logindem{
 	@When("^logingdetails \"([^\"]*)\" and \"([^\"]*)\"$")
 	public void logingdetails_and(String arg1, String arg2) throws Throwable {
 	 //arg1.trim();
-	 //arg2.trim();
+	 //arg2.trim()s;
 		//String login="admin";
 		  //  String password1="Admin@987";
-		   // if(login.equals(arg1) && password1.equals(arg2) )
+		   // if(login.equals(arg1) && 1password1.equals(arg2) )
 		   // {
 		    	dr.findElement(By.id("txtUserName")).sendKeys(arg1);
 		    	dr.findElement(By.id("txtPassword")).sendKeys(arg2);
